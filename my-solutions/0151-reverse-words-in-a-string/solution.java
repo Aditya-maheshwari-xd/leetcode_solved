e9a067c1d9c1c31884b0ce9a5636b21b581c8088
Aditya-maheshwari-xd <aditya1243567@gmail.com>
2025-11-04 11:35:37 +0000

    [LeetCode Sync] Runtime - 6 ms (80.24%), Memory - 44.2 MB (80.10%)

---
+++ my-solutions/0151-reverse-words-in-a-string/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" +");
+        StringBuilder a = new StringBuilder();
+        for(int i=words.length-1;i>=0;i--)
+        {
+            a.append(words[i]);
+            a.append(" ");
+        }
+        return a.toString().trim();
+    }
+}
